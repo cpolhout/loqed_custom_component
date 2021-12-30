ac8791f1ba5c8fab233c0dc1012f96b40f1559ff
cpolhout <casper@onegini.com>
2021-12-30 01:32:22 +0100

    Update lock.py

--- custom_components/loqed/lock.py
+++ custom_components/loqed/lock.py
@@ -36,7 +36,6 @@ async def async_setup_entry(
     data = hass.data[DOMAIN][entry.entry_id]
     websession = async_get_clientsession(hass)
     apiclient = loqed.APIClient(websession, LOQED_URL, data["api_key"])
-    # eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5NGRiMDgyOS00ZWY4LTQ4ODEtYjY4YS05NGZkOTY1NWY0MzgiLCJqdGkiOiI1NDI5MzI2OTRlMmMxYzgxODk3YzU1YzE1YzZlOTMyYmE4MDdhYWFmNWEyNTQ4ZjhiMWExMTBlNjRkMzY4MmVjYzdhNmU2NTBlMWM5Yzc5NyIsImlhdCI6MTYzNzAyMDcxNy42NjM5NTcsIm5iZiI6MTYzNzAyMDcxNy42NjQyNDcsImV4cCI6MTY1MjY1OTExNS4yOTA4MDYsInN1YiI6IjEwNjEiLCJzY29wZXMiOltdfQ.B7eZUgQjDT6wOfJ6I0LnRa4_2eTEiKkCqrQzXu9dB_eC-ak4yPdxf0YvNhniLNwiS0AxdZq2P2aRlpxlo8g7SwECIz06SqiYHjc26LHraRUwJeXL2y_2beMcm7Xbi9tN_AfNZ0lSh_Sdj1WDDRSTDnsDp7JP2jMrIxVJBMJzhI_traRnTcs_5O41Mlgrg8372HyLWd64QrUDaVQB34tDX6wKpRCSUHeSLJX_DmPM-LUenslGEy_pva0OSQljjqG9LcZY7uC2bcaz6R7ZSDPq1qlsaRTshDZvpRfZ-bB1S0yw6wpTZ4Y2bhNbXHtZmejRLfiRn2JjHTisCQwxWaKYM-1EfemMXP5AZPstwOGTdB0vUBqsTtYHPpEEHgm94oznVlPRtjLu0uWYUXT07PFmZtDlXp6GD6gM1ZP376-nXL0kjimfhAntcSsxKGiJg7yw4hQt6oID50o3kk2_nbeIiCEIYMmeiSgGe2XNMo11SR_gn60jHGWJT6YrukkB5Ywqpnw_xqi2TQwIEydhJMqoHjJyTju_0vTlKh8LCXC064DIyU6QhMG5QOPMJ3xKgGdsg1rTHVh3vd39Y42SDQ3yj_CQHuvQsmjP-fSgJ-BVepJk4Ae5VdFTb49w7qNPg91pPCr0EGbHA54kGIeTdemsTLnj7A1MEjO64Sj9jO8V6FE
     api = loqed.LoqedAPI(apiclient)
     loqed_locks = await api.async_get_locks()
     print(f"The lock name: {loqed_locks[0].name}")
@@ -73,17 +72,14 @@ class LoqedLock(LockEntity):
         self._attr_name = self._lock.name
         self._bolt_state = self._lock.bolt_state
         self._attr_supported_features = SUPPORT_OPEN
+        if self._bolt_state == "night_lock":
+            self._state = STATE_LOCKED
+        else:
+            self._state = STATE_UNLOCKED
         # self._data = data
         # self._device = device
         # self._lock_status = None
         # self._attr_name = device.device_name
-
-    @property
-    def state(self):
-        if self._bolt_state == "night_lock":
-            return STATE_LOCKED
-        else:
-            return STATE_UNLOCKED
 
     @property
     def is_locking(self):
@@ -105,18 +101,17 @@ class LoqedLock(LockEntity):
         """Return true if lock is locked."""
         return self._state == STATE_LOCKED
 
+    # @property
+    # def state(self):
+    #     if self._bolt_state == "night_lock":
+    #         return STATE_LOCKED
+    #     else:
+    #         return STATE_UNLOCKED
+
     @property
     def supported_features(self):
         """Flag supported features."""
         return SUPPORT_OPEN
-
-    # @property
-    # def extra_state_attributes(self):
-    #     """Return entity specific state attributes."""
-    #     state_attr = {}
-    #     for prop, val in vars(self._lock).items():
-    #         state_attr[prop] = val
-    #     return state_attr
 
     @property
     def extra_state_attributes(self):
@@ -135,11 +130,13 @@ class LoqedLock(LockEntity):
         return state_attr
 
     async def async_lock(self, **kwargs):
+        self._state = STATE_LOCKING
         await self._lock.lock()
         self._state = STATE_LOCKED
         self.async_write_ha_state()
 
     async def async_unlock(self, **kwargs):
+        self._state = STATE_UNLOCKING
         await self._lock.unlock()
         self._state = STATE_UNLOCKED
         self.async_write_ha_state()
@@ -152,11 +149,18 @@ class LoqedLock(LockEntity):
     async def async_update(self) -> None:
         """Update the internal state of the device."""
         await self._lock.update()
+        self._attr_unique_id = self._lock.id
+        self._attr_name = self._lock.name
+        self._bolt_state = self._lock.bolt_state
+        if self._bolt_state == "night_lock":
+            self._state = STATE_LOCKED
+        else:
+            self._state = STATE_UNLOCKED
         # locks = await api.async_get_locks()
         # # self._nickname = self._sesame.nickname
         # self._device_id = str(self._sesame.id)
         # self._serial = self._sesame.serial
         # self._battery = status["battery"]
-        self._state = self._lock.bolt_state
-        self._battery = self._lock.battery_percentage
-        self._attr_name = self._lock.name
+        # self._state = self._lock.bolt_state
+        # self._battery = self._lock.battery_percentage
+        # self._attr_name = self._lock.name
