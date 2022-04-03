97ac8b42b74f5fcf3b61696518375ecf01485d70
cpolhout <casper@onegini.com>
2022-04-03 14:13:25 +0000

    Updated Custom component

--- custom_components/loqed/config_flow.py
+++ custom_components/loqed/config_flow.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import logging
 from typing import Any
 import re
+import json
 import aiohttp
 import voluptuous as vol
 from homeassistant import config_entries
@@ -31,21 +32,20 @@ urlRegex = re.compile(
 
 async def validate_input(hass, data):
     """Validate the user input allows us to connect.
-    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
+    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user. The config-key is a json object.
     """
-    # check local HA URL
     if len(data["internal_url"]) > 5:
         if re.match(urlRegex, data["internal_url"]) is None:
             _LOGGER.error("Local HA URL is incorrect %s", data["internal_url"])
             raise ValueError("Local HA URL is incorrect")
-
     newdata = data
-    vals = data["config"].split("|")
-    newdata["ip"] = vals[0]
-    newdata["host"] = vals[1]
-    newdata["api-key"] = vals[2]
-    newdata["key-id"] = vals[3]
-    newdata["bkey"] = vals[4]
+    json_config = json.loads(data["config"])
+    newdata["ip"] = json_config.bridge_ip
+    newdata["host"] = json_config.bridge_mdns_hostname
+    newdata["bkey"] = json_config.bridge_key
+    newdata["key_id"] = int(json_config.lock_key_local_id)
+    newdata["api_key"] = json_config.lock_key_key
+    _LOGGER.debug("Got Info from config: %s", str(newdata))
 
     # 1. Checking loqed-connection
     try:
@@ -53,7 +53,7 @@ async def validate_input(hass, data):
             apiclient = loqed.APIClient(session, "http://" + newdata["ip"])
             api = loqed.LoqedAPI(apiclient)
             lock = await api.async_get_lock(
-                newdata["api-key"], newdata["bkey"], newdata["key-id"], newdata["name"]
+                newdata["api_key"], newdata["bkey"], newdata["key_id"], newdata["name"]
             )
             _LOGGER.debug("Lock details retrieved: %s", newdata["ip"])
             newdata["id"] = lock.id
@@ -65,13 +65,6 @@ async def validate_input(hass, data):
     except Exception:
         _LOGGER.error("HTTP Connection error to loqed lock: %s:%s", lock.name, lock.id)
         raise CannotConnect from aiohttp.ClientError
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-
-    # Return info that you want to store in the config entry.
     return newdata
 
 
@@ -85,8 +78,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_zeroconf(self, discovery_info) -> FlowResult:
         """Handle zeroconf discovery."""
-        # Hostname is format: LOQED-
-        # self.host = discovery_info.hostname.rstrip(".")
         _LOGGER.debug("HOST: %s", discovery_info.hostname)
         _LOGGER.info("HOST: %s", discovery_info.hostname)
 
@@ -97,40 +88,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             lock_data = await api.async_get_lock_details()
 
         # Check if already exists
-        await self.async_set_unique_id(
-            lock_data["bridge_mac_wifi"], raise_on_progress=False
-        )
-        self._abort_if_unique_id_configured()
-
-        # try:
-        #     self.brother = Brother(self.host, snmp_engine=snmp_engine, model=model)
-        #     await self.brother.async_update()
-        # except UnsupportedModel:
-        #     return self.async_abort(reason="unsupported_model")
-        # except (ConnectionError, SnmpError):
-        #     return self.async_abort(reason="cannot_connect")
-
-        # Check if already configured
-        # await self.async_set_unique_id(self.brother.serial.lower())
-        # self._abort_if_unique_id_configured()
-
-        # self.context.update(
-        #     {
-        #         "title_placeholders": {
-        #             "serial_number": self.brother.serial,
-        #             "model": self.brother.model,
-        #         }
-        #     }
-        # )
+        if await self.async_set_unique_id(lock_data["bridge_mac_wifi"]) is not None:
+            self.async_abort(reason="already_configured")
         self.host = host
         return await self.async_step_user()
-
-    # async def async_step_zeroconf(self, discovery_info: dict):
-    #     print("DISCOVERED:")
-    #     print(discovery_info)
-    #     # await self.async_set_unique_id(int(discovery_info.get(HOSTNAME)[12:], 16))
-    #     # self._abort_if_unique_id_configured()
-    #     return await self.async_step_user()
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -143,17 +104,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 internal_url = "http://<IP>:8123"
         except network.NoURLAvailableError:
             internal_url = "http://<IP>:8123"
-
-        # STEP_USER_DATA_SCHEMA = vol.Schema(
-        #     {
-        #         vol.Required("name", default="My Lock"): str,
-        #         vol.Required("ip", default=self.host): str,
-        #         vol.Required("api-key"): str,
-        #         vol.Required("key-id", default=None): int,
-        #         vol.Required("bkey"): str,
-        #         vol.Required("internal_url", default=internal_url): str,
-        #     }
-        # )
 
         STEP_USER_DATA_SCHEMA = vol.Schema(
             {
@@ -174,9 +124,11 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         try:
             info = await validate_input(self.hass, user_input)
-            await self.async_set_unique_id(info["id"], raise_on_progress=False)
-            self._abort_if_unique_id_configured()
-            print("IN CONFIG 3, Unique ID set, info:" + str(info))
+            if await self.async_set_unique_id(info["id"]) is not None:
+                _LOGGER.error(
+                    "Aborting config: This device with this mac is already configured"
+                )
+                return self.async_abort(reason="already_configured")
             return self.async_create_entry(
                 title="LOQED Touch Smart Lock", data=user_input
             )
@@ -191,12 +143,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
-
-    # @staticmethod
-    # @callback
-    # def async_get_options_flow(config_entry):
-    #     """Get the options flow for this handler."""
-    #     return OptionsFlowHandler(config_entry)
 
 
 class CannotConnect(HomeAssistantError):
